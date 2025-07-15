f62e0d16e096680ab48976ab9e249c4858f88266
xuannpp <xuannpp@vng.com.vn>
2025-07-15 12:31:08 +0700

    config : amount calling LLM

--- internal/constants/llm_call_amout.go
+++ internal/constants/llm_call_amout.go
@@ -6,12 +6,12 @@ type LLMCallAmount struct {
 }
 
 var AmountMap = map[string]LLMCallAmount{
-	F1_SUGGEST_EXAM:                {Amount: 500, Desc: "F1 Suggest Exam"},
-	F1_SUGGEST_OUTLINES:            {Amount: 500, Desc: "F1 Suggest Outlines"},
-	F1_SUGGEST_QUESTIONS:           {Amount: 500, Desc: "F1 Suggest Questions"},
+	F1_SUGGEST_EXAM:                {Amount: 5, Desc: "F1 Suggest Exam"},
+	F1_SUGGEST_QUESTIONS:           {Amount: 5, Desc: "F1 Suggest Questions"},
+	F1_SUGGEST_OUTLINES:            {Amount: 0, Desc: "F1 Suggest Outlines"},
 	F2_SCORE:                       {Amount: 0, Desc: "F2 Score"},
 	F3_SUGGEST_INTERVIEW_QUESTIONS: {Amount: 0, Desc: "F3 Suggest Interview Questions"},
-	F3_SCORE_INTERVIEW_QUESTIONS:   {Amount: 700, Desc: "F3 Score Interview Questions"},
+	F3_SCORE_INTERVIEW_QUESTIONS:   {Amount: 0, Desc: "F3 Score Interview Questions"},
 }
 
 func GetLLMCallAmount(key string) (float32, string) {
