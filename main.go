b70fc80741acfa13e776be6f2e0c1ddf95b53ba9
xuannpp <xuannpp@vng.com.vn>
2025-07-19 00:59:46 +0700

    t

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "darius/cmd"
+import (
+	"darius/cmd"
+
+	"go.uber.org/zap"
+)
 
 func main() {
+	logger, _ := zap.NewDevelopment() // hoặc zap.NewProduction()
+	zap.ReplaceGlobals(logger)
 	cmd.Execute()
 }
